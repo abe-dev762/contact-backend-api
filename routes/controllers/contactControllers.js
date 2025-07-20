febf3578feb4eb520981f0fe75b4eff94dee5f42
Ahmad Zaenal Abidin <ahmadabidin762.dev@gmail.com>
2025-07-20 22:12:53 +0800

    first commit

---
+++ routes/controllers/contactControllers.js
@@ -0,0 +1,49 @@
+//Get all contacts
+//@route GET /api/contacts
+//@access public
+const getContacts = (req, res) => {
+    res.status(200).json({message: "Get all contacts"});
+};
+
+//create new contact
+//@route POST /api/contacts
+//@access public
+const createContact = (req, res) => {
+    console.log("The request body is: ", req.body);
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    res.status(201).json({message: "Create contact"});
+};
+
+//get specified contact
+//@route GET /api/contacts/id
+//@access public
+const getContact = (req, res) => {
+    res.status(201).json({message: `Get concact for ${req.params.id}`});
+};
+
+//update contact
+//@route PUT /api/contacts/id
+//@access public
+const updateContact = (req, res) => {
+    res.status(201).json({message: `Update concact for ${req.params.id}`});
+};
+
+//delete contact
+//@route DELETE /api/contacts/id
+//@access public
+const deleteContact = (req, res) => {
+    res.status(201).json({message: `Delete concact for ${req.params.id}`});
+};
+
+
+
+module.exports = { 
+    getContacts, 
+    createContact, 
+    getContact, 
+    updateContact, 
+    deleteContact };
